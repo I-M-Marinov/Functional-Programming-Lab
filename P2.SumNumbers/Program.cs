7d7aee9d54509f3811ab883ba027b6bd5481e5ba
I-M-Marinov <lcfrrr@gmail.com>
2023-09-26 21:09:32 +0300

    Added solution to P02. Sum Numbers
    Added solution to P02. Sum Numbers

--- P2.SumNumbers/Program.cs
+++ P2.SumNumbers/Program.cs
@@ -1,2 +1,15 @@
 ﻿
+int Sum (List<int> numbers)
+{
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+    return sum;
+}
+
+List<int> inputNumbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+Console.WriteLine(inputNumbers.Count);
+Console.WriteLine($"{Sum(inputNumbers)}");
 
