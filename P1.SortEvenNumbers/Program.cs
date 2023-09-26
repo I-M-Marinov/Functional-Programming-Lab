b9c56f1316c670a638a6fa4d817c3ec9562f8783
I-M-Marinov <lcfrrr@gmail.com>
2023-09-26 20:53:53 +0300

    Solution P1. Sort Even Numbers
    Solution P1. Sort Even Numbers

--- P1.SortEvenNumbers/Program.cs
+++ P1.SortEvenNumbers/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿ 
+
+List<int> inputNumbers = Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+
+Console.WriteLine(String.Join(", ", inputNumbers));
+
