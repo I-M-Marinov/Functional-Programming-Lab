d86da680eae9ad96f5f62bfe2fde6b736800c203
I-M-Marinov <lcfrrr@gmail.com>
2023-09-28 01:21:41 +0300

    Update Program.cs

--- P3.CountUppercaseWords/Program.cs
+++ P3.CountUppercaseWords/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-Predicate<string> checker = n => n[0] == n.ToUpper()[0]; // gets a string returns a bool (( True / False ))
+﻿Predicate<string> checker = n => n[0] == n.ToUpper()[0]; // gets a string returns a bool (( True / False ))
 
 string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(w => checker(w)).ToArray();
 
