fddc010023e1137f36e04082027ab03b212a2b11
I-M-Marinov <lcfrrr@gmail.com>
2023-09-27 13:36:22 +0300

    Added solutions to P3, P4, P5
    Added solutions to
    P3 Count Uppercase Words
    P4 Add VAT
    P5 Filter By Age

---
+++ P3.CountUppercaseWords/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Predicate<string> checker = n => n[0] == n.ToUpper()[0]; // gets a string returns a bool (( True / False ))
+
+string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(w => checker(w)).ToArray();
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+
---
+++ P4.AddVAT/Program.cs
@@ -0,0 +1,12 @@
+﻿double AddVat(double price)
+{
+    price += price * 0.20;
+    return price;
+}
+
+double[] inputPrices = Console.ReadLine().Split(", ").Select(double.Parse).ToArray();
+
+foreach (double price in inputPrices)
+{
+    Console.WriteLine($"{AddVat(price):F2}");
+}
